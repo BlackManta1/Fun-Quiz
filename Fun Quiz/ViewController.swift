7e83f0bc25096d4e1f2091b559e8ce5c1cc4b4c6
Saliou DJALO <djalosaliou@gmail.com>
2019-01-03 20:02:30 +0400

    Initial Commit

---
+++ Fun Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fun Quiz
+//
+//  Created by Saliou DJALO on 03/01/2019.
+//  Copyright © 2019 Saliou DJALO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
